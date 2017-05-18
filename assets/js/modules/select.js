2780c30a9f0b9ad85b554ac82ddc622595cc6557
tabuna <bliz48rus@gmail.com>
2017-05-18 17:46:44 +0300

    up

--- assets/js/modules/select.js
+++
@@ -1,5 +0,0 @@
-$(function () {
-    $(".select2").select2({
-        width: '100%'
-    });
-});
--- assets/webpack.mix.js
+++ assets/webpack.mix.js
@@ -16,10 +16,8 @@ mix.less('./less/app.less', '../dist/css/orchid.css');
 
 mix.copy('./img/', 'dist/img');
 mix.copy('./node_modules/bootstrap/dist/fonts/', '../dist/fonts');
-mix.copy('./node_modules/font-awesome/fonts/', '../dist/fonts');
 mix.copy('./node_modules/simple-line-icons/fonts/', '../dist/fonts');
-mix.copy('./node_modules/summernote/dist/font/', '../dist/fonts');
-mix.copy('./node_modules/summernote/dist/', '../dist/summernote');
+
 
 
 mix.js([
